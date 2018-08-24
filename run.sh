b0846a2dc077c6c9c40b919b6f7d68dd1b4a98a2
Gopal V <gopalv@apache.org>
2018-08-24 01:00:06 -0700

    add a few more comments

--- run.sh
+++ run.sh
@@ -1,3 +1,5 @@
+# 4 threads with a ramp-up of 500ms
+# run all queries in queries32-4.txt once
 java -jar target/jdbc-bench-0.0.1-SNAPSHOT.jar \
 -u 'jdbc:hive2://localhost:10007/tpcds_bin_partitioned_orc_10000;transportMode=binary;httpPath=cliservice' \
  -t 4\
